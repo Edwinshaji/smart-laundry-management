7d864a9f9142d2eb0b2c63243957fae33f660537
Edwin Shaji <edwinshajioffical@gmail.com>
2026-02-20 22:48:41 +0530

    production cors settings

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -167,13 +167,9 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # CORS / CSRF configuration
-# CORS_ALLOWED_ORIGINS = [
-#     "http://localhost:5173",
-#     "http://127.0.0.1:5173",
-#     "http://localhost:3000",
-#     "http://127.0.0.1:3000",
-# ]
-CORS_ALLOW_ALL_ORIGINS=True
+CORS_ALLOWED_ORIGINS = [
+    "https://smart-laundry-management.vercel.app"
+]
 
 # Allow cookies to be sent with cross-origin requests
 CORS_ALLOW_CREDENTIALS = True
@@ -194,10 +190,7 @@ CSRF_COOKIE_HTTPONLY = False
 
 # IMPORTANT: Trust the frontend origin for CSRF
 CSRF_TRUSTED_ORIGINS = [
-    # "http://localhost:5173",
-    # "http://127.0.0.1:5173",
-    # "http://localhost:3000",
-    # "http://127.0.0.1:3000",
+    "https://smart-laundry-management.vercel.app"
 ]
 
 REST_FRAMEWORK = {
